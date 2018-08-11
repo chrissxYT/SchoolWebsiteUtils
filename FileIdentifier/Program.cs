45822c160da3cfa6f09bf03e73a28d28be84b0ab
chrissx <christian.chrissicx2004@freenet.de>
2018-08-11 20:15:39 +0200

    Initial commit

---
+++ FileIdentifier/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FileIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = Console.ReadLine();
+            foreach(string f in Directory.GetFiles(dir, "*.data"))
+            {
+                string s = File.ReadAllText(f);
+                if (s.StartsWith("<html") || s.ToLower().StartsWith("<!doctype html") || (s.StartsWith("<?xml") && s.Contains("<html")))
+                    File.Move(f, Path.ChangeExtension(f, "html"));
+                else if (s.Substring(0, 10).Contains("PDF"))
+                    File.Move(f, Path.ChangeExtension(f, "pdf"));
+                else if (s.Substring(0, 10).Contains("JFIF"))
+                    File.Move(f, Path.ChangeExtension(f, "jpg"));
+                else if (s.Substring(5, 15).Contains("AVI"))
+                    File.Move(f, Path.ChangeExtension(f, "avi"));
+                else if (s.Substring(10, 20).Contains("mp4"))
+                    File.Move(f, Path.ChangeExtension(f, "mp4"));
+            }
+        }
+    }
+}
---
+++ WebsiteDump/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+
+namespace WebsiteDump
+{
+    static class Program
+    {
+        static List<id_site> known_sites = new List<id_site>();
+        static string pics = "https://gymnasium-pegnitz.de/eigene/bildergalerie/show_pictures.php?link=";
+        static string sites = "https://gympeg.de/index.php?id=";
+
+        static void Main(string[] args)
+        {
+            for (short i = 0; i < 10000; i++)
+            {
+                check(i);
+            }
+            GC.Collect();
+            foreach (id_site s in known_sites)
+            {
+                File.WriteAllBytes(s.ids.First() + ".data", s.bytes);
+                File.WriteAllLines(s.ids.First() + ".ids", s.ids.to_string_array());
+            }
+        }
+
+        static string[] to_string_array<T>(this List<T> l)
+        {
+            string[] s = new string[l.Count];
+            for(int i = 0; i < l.Count; i++)
+            {
+                s[i] = l[i].ToString();
+            }
+            return s;
+        }
+
+        static void check(int num)
+        {
+            try
+            {
+                HttpClient hc = new HttpClient();
+                HttpResponseMessage r = hc.GetAsync(pics + num).Result;
+                byte[] content = r.Content.ReadAsByteArrayAsync().Result;
+                int i = known_sites.FindIndex((s) => arr_equ(s.bytes, content));
+                if (i == -1)
+                {
+                    known_sites.Add(new id_site(num, content));
+                }
+                else
+                {
+                    known_sites[i].ids.Add(num);
+                }
+                Console.WriteLine(num);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(num + " has " + e.GetType());
+                check(num);
+            }
+        }
+
+        static bool arr_equ(byte[] left, byte[] right)
+        {
+            if (left.LongLength != right.LongLength)
+                return false;
+            for(long i = 0; i < left.LongLength; i++)
+            {
+                if (left[i] != right[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    struct id_site
+    {
+        public List<int> ids;
+        public byte[] bytes;
+
+        public id_site(int id, byte[] bytes)
+        {
+            ids = new List<int>();
+            ids.Add(id);
+            this.bytes = bytes;
+        }
+    }
+}
